4708fe7aa1b9e2c33d3ff67908ce0337a77069ed
Serdar <serdar2225@gmail.com>
2020-06-07 00:23:16 -0400

    two new example classes have been created

---
+++ src/test/java/testNgTest.java
@@ -0,0 +1,11 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class testNgTest {
+    @Test
+    public void test(){
+        boolean someCondition = true;
+        Assert.assertTrue( someCondition );
+
+    }
+}
