5ef172dce8df7193813c98b3bcbde695b0248857
Serdar <serdar2225@gmail.com>
2020-06-07 00:11:26 -0400

    pom cml has been updated

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -1,10 +1,25 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class MyFirstTest {
+    private WebDriver driver;
+    @BeforeClass
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\suler\\Desktop\\Selenium\\chromedriver\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @AfterClass
+    public void quit(){
+        driver.quit();
+    }
+
     @Test
     public void test(){
-        boolean someCondition = true;
-        Assert.assertTrue( someCondition );
+        driver.get( "https://google.com" );
     }
 }
